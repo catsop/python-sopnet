5ecdf2cdef5767527a1a9e28e9b469803fedd35b
Larry Lindsey <llindsey@clm.utexas.edu>
2014-03-17 19:17:27 -0500

    SegmentReader: Features fetching moved to SegmentFeatureReader

--- sopnet/catmaid/persistence/SegmentReader.cpp
+++ sopnet/catmaid/persistence/SegmentReader.cpp
@@ -10,7 +10,6 @@ SegmentReader::SegmentReader()
 	registerInput(_blocks, "blocks");
 	registerInput(_store, "store");
 	registerOutput(_segments, "segments");
-	registerOutput(_features, "features");
 }
 
 void
@@ -22,43 +21,6 @@ SegmentReader::updateOutputs()
 	LOG_DEBUG(segmentreaderlog) << "Reading segments from " << *_blocks << std::endl;
 	segments = _store->retrieveSegments(_blocks);
 	LOG_DEBUG(segmentreaderlog) << "Read " << segments->size() << " segments" << std::endl;
-	features = reconstituteFeatures(segments);
-	LOG_DEBUG(segmentreaderlog) << "Read " << features->size() << " features" << std::endl;
 	
-	*_features = *features;
 	*_segments = *segments;
-}
-
-
-pipeline::Value<Features>
-SegmentReader::reconstituteFeatures(pipeline::Value<Segments> segments)
-{
-	pipeline::Value<Features> features;
-	pipeline::Value<SegmentStore::SegmentFeaturesMap> featureMap;
-	SegmentStore::SegmentFeaturesMap::iterator it;
-	std::vector<std::string> featureNames;
-	std::map<unsigned int, unsigned int> idMap;
-	
-	unsigned int i = 0;
-
-	featureMap = _store->retrieveFeatures(segments);
-	featureNames = _store->getFeatureNames();
-	
-	features->resize(featureMap->size(), featureNames.size());
-	
-	foreach (std::string name, featureNames)
-	{
-		features->addName(name);
-	}
-	
-	for (it = featureMap->begin(); it != featureMap->end(); ++it)
-	{
-		idMap[it->first->getId()] = i;
-		(*features)[i] = it->second;
-		++i;
-	}
-	
-	features->setSegmentIdsMap(idMap);
-	
-	return features;
 }
