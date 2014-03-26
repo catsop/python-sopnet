9eef62225de95db61c49b7b47a42be9780edfae4
Larry Lindsey <llindsey@clm.utexas.edu>
2014-03-26 10:42:44 -0500

    CostReader, CostWriter: LinearObjective persistence in SegmentStore

---
+++ sopnet/catmaid/persistence/CostReader.h
@@ -0,0 +1,32 @@
+#ifndef COST_READER_H__
+#define COST_READER_H__
+
+#include <pipeline/all.h>
+#include <sopnet/segments/Segments.h>
+#include <inference/LinearObjective.h>
+#include <catmaid/persistence/SegmentStore.h>
+
+class CostReader : public pipeline::SimpleProcessNode<>
+{
+public:
+	/**
+	 * Construct a CostReader, which retrieves LinearObjectives from a SegmentStore
+	 * Inputs:
+	 *   SegmentStore - "store"
+	 *   Segments     - "segment"
+	 *   double       - "default cost" - Optional
+	 * Outputs:
+	 *   LinearObjective - "objective"
+	 */
+	CostReader();
+	
+private:
+	void updateOutputs();
+	
+	pipeline::Input<SegmentStore> _store;
+	pipeline::Input<Segments> _segments;
+	pipeline::Input<double> _defaultCost;
+	pipeline::Output<LinearObjective> _objective;
+};
+
+#endif //COST_READER_H__
