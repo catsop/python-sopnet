f644ed2c6204dd607602e73a852263181bb50cd3
Larry Lindsey <llindsey@clm.utexas.edu>
2014-03-28 17:23:34 -0500

    CoreManager: fixed bugs in coresInBox

--- sopnet/sopnet/block/CoreManager.cpp
+++ sopnet/sopnet/block/CoreManager.cpp
@@ -1,4 +1,7 @@
 #include "CoreManager.h"
+#include <util/Logger.h>
+
+logger::LogChannel coremanagerlog("coremanagerlog", "[CoreManager] ");
 
 CoreManager::CoreManager(const boost::shared_ptr<BlockManager> blockManager,
 						 const util::point3<unsigned int> coreSizeInBlocks) :
@@ -36,20 +39,15 @@ CoreManager::coreAtCoordinates(const util::point3<unsigned int> coordinates)
 {
 	if (_coreMap.count(coordinates))
 	{
+		LOG_DEBUG(coremanagerlog) << "Found core at coordinates: " << coordinates << std::endl;
 		return _coreMap[coordinates];
 	}
 	else
 	{
 		boost::shared_ptr<Core> core;
-		util::point3<unsigned int> blockCoordinates = util::point3<unsigned int>(coordinates);
-		util::point3<unsigned int> expandBy = _coreSizeInBlocks - util::point3<unsigned int>(1, 1, 1);
-		boost::shared_ptr<Blocks> blocks = boost::make_shared<Blocks>();
-		boost::shared_ptr<Block> block;
-		blockCoordinates *= _coreSizeInBlocks;
-		
-		block = _blockManager->blockAtCoordinates(blockCoordinates);
-		blocks->add(block);
-		blocks->expand(expandBy);
+		util::point3<unsigned int> coreLocation = coordinates * _coreSize;
+		boost::shared_ptr<Box<> > coreBox = boost::make_shared<Box<> >(coreLocation, _coreSize);
+		boost::shared_ptr<Blocks> blocks = _blockManager->blocksInBox(coreBox);
 		
 		core = boost::make_shared<Core>(_lastId++, blocks, shared_from_this());
 		
@@ -69,23 +67,36 @@ CoreManager::getBlockManager()
 boost::shared_ptr<Cores>
 CoreManager::coresInBox(const boost::shared_ptr<Box<> > box)
 {
+	LOG_DEBUG(coremanagerlog) << "coresInBox called on " << *box << std::endl;
+	
+	LOG_DEBUG(coremanagerlog) << "block size: " << _blockManager->blockSize() << std::endl;
+	LOG_DEBUG(coremanagerlog) << "core size: " << _coreSize << std::endl;
+	
+	
 	util::point3<unsigned int> corner = box->location();
 	util::point3<unsigned int> size = box->size();
 	boost::shared_ptr<Cores> cores = boost::make_shared<Cores>();
+	
+	LOG_DEBUG(coremanagerlog) << "Here we go " << *box << std::endl;
 	
 	for (unsigned int z = corner.z; z - corner.z < size.z; z += _coreSize.z)
 	{
 		for (unsigned int y = corner.y; y - corner.y < size.y; y += _coreSize.y)
 		{
-			for (unsigned int x = corner.x; x - corner.x < size.x; x += _coreSize.z)
+			for (unsigned int x = corner.x; x - corner.x < size.x; x += _coreSize.x)
 			{
 				util::point3<unsigned int> coords = point3<unsigned int>(x, y, z) / _coreSize;
+				LOG_DEBUG(coremanagerlog) << "Looking for core at coordinates" << coords << std::endl;
 				boost::shared_ptr<Core> core = coreAtCoordinates(coords);
 				cores->add(core);
 			}
 		}
 	}
 
+	LOG_DEBUG(coremanagerlog) << "Returning " << cores->length() << " cores." << std::endl;
+	
+	LOG_DEBUG(coremanagerlog) << "Cores contains" << cores->asBlocks()->length() << " blocks." << std::endl;
+	
 	return cores;
 }
 
