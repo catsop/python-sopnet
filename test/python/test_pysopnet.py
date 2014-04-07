f5e0e7188408c842e3aa784c3a986651fca2a5d5
Jan Funke <funke@ini.ch>
2014-04-07 11:36:06 +0200

    added another test case for SliceGuarantor to test_pysopnet.py

--- test/python/test_pysopnet.py
+++ test/python/test_pysopnet.py
@@ -19,20 +19,36 @@ config.setBlockSize(ps.point3(16, 16, 8))
 bs = config.getBlockSize()
 print "[" + str(bs.x) + ", " + str(bs.y) + ", " + str(bs.z) + "]"
 
+print
+print
 print "Creating a SliceGuarantor"
 
 sliceGuarantor = ps.SliceGuarantor()
 
-print "Requesting a block"
+print
+print
+print "Requesting block (0,0,0)"
 
 request = ps.point3(0,0,0)
 parameters = ps.SliceGuarantorParameters()
 sliceGuarantor.fill(request, parameters, config)
 
+print
+print
+print "Requesting block (0,0,20)"
+
+request = ps.point3(0,0,20)
+parameters = ps.SliceGuarantorParameters()
+sliceGuarantor.fill(request, parameters, config)
+
+print
+print
 print "Creating a SegmentGuarantor"
 
 segmentGuarantor           = ps.SegmentGuarantor()
 
+print
+print
 print "Requesting a block"
 
 segmentGuarantorParameters = ps.SegmentGuarantorParameters()
@@ -42,10 +58,14 @@ print "Missing points:"
 for p in missing:
   print "[" + str(p.x) + ", " + str(p.y) + ", " + str(p.z) + "]"
 
+print
+print
 print "Creating a SolutionGuarantor"
 
 solutionGuarantor           = ps.SolutionGuarantor()
 
+print
+print
 print "Requesting a block"
 
 solutionGuarantorParameters = ps.SolutionGuarantorParameters()
