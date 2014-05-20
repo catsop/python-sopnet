75861be3e2cd79ab211b63bd2813a5f58fa3bf9a
Larry Lindsey <llindsey@clm.utexas.edu>
2014-05-20 11:19:31 -0500

    CatsopTest: TestSuite

--- binaries/tests/CatsopTest.h
+++ binaries/tests/CatsopTest.h
@@ -2,6 +2,8 @@
 #define TEST_CATSOP_H__
 
 #include <string>
+#include <vector>
+#include <boost/shared_ptr.hpp>
 
 /**
  * A really simple unit test designed for the catsop project.
@@ -15,7 +17,7 @@ class Test
 public:
 	/**
 	 * Run this test.
-	 * @return true for a successfult test, false otherwise.
+	 * @return true for a successful test, false otherwise.
 	 */
 	virtual bool test() = 0;
 	
@@ -28,6 +30,30 @@ public:
 	 * @return a string describing the reason for a failure, if one occurred.
 	 */
 	virtual std::string reason() = 0;
+};
+
+//TODO: consider a dependency graph implementation. Use sopnet pipeline if this is necessary.
+
+class TestSuite
+{
+public:
+	TestSuite(const std::string& name);
+	
+	/**
+	 * Add a Test to this TestSuite. Tests are executed in the order in which they are
+	 * added.
+	 */
+	void addTest(const boost::shared_ptr<Test> test);
+	
+	/**
+	 * Run all tests.
+	 * @return true if all tests pass, false when one fails.
+	 */
+	bool runAll();
+	
+private:
+	std::string _name;
+	std::vector<boost::shared_ptr<Test> > _tests;
 };
 
 };
