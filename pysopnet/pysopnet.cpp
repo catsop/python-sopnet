b81917bd91f82fdb964cebeece6fa8d3b0572892
Jan Funke <funke@ini.ch>
2014-06-19 17:32:11 -0400

    added {s,g}etCatmaidHost to python wrapper

--- pysopnet/pysopnet.cpp
+++ pysopnet/pysopnet.cpp
@@ -64,6 +64,8 @@ BOOST_PYTHON_MODULE(libpysopnet) {
 			.def("getCatmaidProjectId", &ProjectConfiguration::getCatmaidProjectId)
 			.def("setDjangoUrl", &ProjectConfiguration::setDjangoUrl)
 			.def("getDjangoUrl", &ProjectConfiguration::getDjangoUrl, boost::python::return_value_policy<boost::python::copy_const_reference>())
+			.def("setCatmaidHost", &ProjectConfiguration::setCatmaidHost)
+			.def("getCatmaidHost", &ProjectConfiguration::getCatmaidHost, boost::python::return_value_policy<boost::python::copy_const_reference>())
 			.def("setBlockSize", &ProjectConfiguration::setBlockSize)
 			.def("getBlockSize", &ProjectConfiguration::getBlockSize, boost::python::return_internal_reference<>())
 			.def("setCoreSize", &ProjectConfiguration::setCoreSize)
