09e918406c30d7715b5cfe48dccd7e3e1c6b06c0
Jan Funke <funke@ini.ch>
2014-06-19 20:18:01 -0400

    simplified test/python/test_pysopnet.py

--- test/python/test_pysopnet.py
+++ test/python/test_pysopnet.py
@@ -4,72 +4,29 @@ import libpysopnet as ps
 
 print "Increasing wrapper verbosity"
 
-ps.setLogLevel(4);
+ps.setLogLevel(3);
 
 print "Creating a poject configuration"
 
 config = ps.ProjectConfiguration()
 config.setBackendType(ps.BackendType.Local)
 
-print "setting Django URL"
-config.setDjangoUrl("http://django.url")
-print config.getDjangoUrl()
-
 print "setting volume size"
 config.setVolumeSize(ps.point3(1024, 1024, 20))
 
 print "setting block size"
-config.setBlockSize(ps.point3(16, 16, 8))
+config.setBlockSize(ps.point3(512, 512, 10))
 bs = config.getBlockSize()
 print "[" + str(bs.x) + ", " + str(bs.y) + ", " + str(bs.z) + "]"
 
 print "setting core size"
-config.setCoreSize(ps.point3(2, 2, 1))
-
-print
-print
-print "Creating a SliceGuarantor"
-
-sliceGuarantor = ps.SliceGuarantor()
-
-print
-print
-print "Requesting block (0,0,0)"
-
-request = ps.point3(0,0,0)
-parameters = ps.SliceGuarantorParameters()
-sliceGuarantor.fill(request, parameters, config)
-
-print
-print
-print "Requesting block (0,0,20)"
-
-request = ps.point3(0,0,20)
-parameters = ps.SliceGuarantorParameters()
-sliceGuarantor.fill(request, parameters, config)
-
-print
-print
-print "Creating a SegmentGuarantor"
-
-segmentGuarantor           = ps.SegmentGuarantor()
-
-print
-print
-print "Requesting a block"
-
-segmentGuarantorParameters = ps.SegmentGuarantorParameters()
-missing = segmentGuarantor.fill(request, segmentGuarantorParameters, config);
-
-print "Missing points:"
-for p in missing:
-  print "[" + str(p.x) + ", " + str(p.y) + ", " + str(p.z) + "]"
+config.setCoreSize(ps.point3(1, 1, 1))
 
 print
 print
 print "Creating a SolutionGuarantor"
 
-solutionGuarantor           = ps.SolutionGuarantor()
+solutionGuarantor = ps.SolutionGuarantor()
 
 print
 print
@@ -77,7 +34,6 @@ print "Requesting a block"
 
 request = ps.point3(0,0,0)
 solutionGuarantorParameters = ps.SolutionGuarantorParameters()
-solutionGuarantorParameters.setCorePadding(2);
 missing = solutionGuarantor.fill(request, solutionGuarantorParameters, config);
 
 print "Missing points:"
