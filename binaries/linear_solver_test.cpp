63b4eddc98e98a1bb310fa4c8389702e1ef51bb8
Jan Funke <funke@ini.ch>
2014-06-23 09:40:21 -0400

    linear_solver_test: catch GRBException (is not std::exception...)

--- binaries/linear_solver_test.cpp
+++ binaries/linear_solver_test.cpp
@@ -2,6 +2,10 @@
 #include <LinearSolverBackend.h>
 #include <DefaultFactory.h>
 
+#ifdef HAVE_GUROBI
+#include "GurobiBackend.h"
+#endif
+
 int main(int, char**) {
 
 	LinearSolverBackend* solver = 0;
@@ -10,6 +14,12 @@ int main(int, char**) {
 
 		DefaultFactory solverFactory;
 		solver = solverFactory.createLinearSolverBackend();
+
+#ifdef HAVE_GUROBI
+	} catch (GRBException& e) {
+
+		std::cerr << "error: " << e.getMessage() << std::endl;
+#endif
 
 	} catch (std::exception& e) {
 
