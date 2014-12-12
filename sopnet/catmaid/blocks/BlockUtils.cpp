catsop/python-sopnet

commit ceaf4c8dd67170145091b98686ff61593b2d351b

Fix wrong image volume causing segfault
When finding the set of blocks containing a bounding box, BlockUtils
used the floor of the min of the box and block size and the ceiling of
the extent of the box and block size, but the difference between the
floor and the min was not considered. In
SegmentGuarantor::computeFeatures this led to cases where the set of
blocks chosen to bound the set of segment slices did not cover the max
of their bounding box. Consequently the image volume retrieved by
StackStore was too small. Consequently
HistogramFeatureExtractor::computeHistogram would access, for some
slices, pixels outside the image volume. However, because there is no
bounds checking, this behavior went unnoticed until it began causing
segfaults do to unrelated changes in memory behavior elsewhere in the
program (the segment store).

sopnet/catmaid/blocks/BlockUtils.cpp
@@ -72,7 +72,8 @@ Blocks
 BlockUtils::getBlocksInBox(const util::box<unsigned int>& box) const {
 
 	util::point3<unsigned int> minBlockCoordinate = box.min/_blockSize;
-	util::point3<unsigned int> numBlocks = (box.size() + _blockSize - util::point3<unsigned int>(1, 1, 1))/_blockSize;
+	util::point3<unsigned int> numBlocks = (box.min*_blockSize - minBlockCoordinate
+			+ box.size() + _blockSize - util::point3<unsigned int>(1, 1, 1))/_blockSize;
 
 	return collectBlocks(minBlockCoordinate, numBlocks);
 }

commit 7413a67aaa8dc19b33b918fb91c4fd52eed70eaa

Fix getBlocksInBox integer ceiling division
A misplaced term from ceaf4c8dd67170145091b98686ff61593b2d351b caused
enormous numbers of blocks to be returned.

sopnet/catmaid/blocks/BlockUtils.cpp
@@ -72,7 +72,7 @@ Blocks
 BlockUtils::getBlocksInBox(const util::box<unsigned int>& box) const {
 
 	util::point3<unsigned int> minBlockCoordinate = box.min/_blockSize;
-	util::point3<unsigned int> numBlocks = (box.min*_blockSize - minBlockCoordinate
+	util::point3<unsigned int> numBlocks = (box.min - minBlockCoordinate*_blockSize
 			+ box.size() + _blockSize - util::point3<unsigned int>(1, 1, 1))/_blockSize;
 
 	return collectBlocks(minBlockCoordinate, numBlocks);