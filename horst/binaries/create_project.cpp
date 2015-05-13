36f8822903e3a22cd7199c1b8c5bc6f8caa127b6
Jan Funke <funke@ini.ch>
2015-05-13 16:43:59 -0400

    horst: added option to read raw stack from catsop project

--- horst/binaries/create_project.cpp
+++ horst/binaries/create_project.cpp
@@ -15,6 +15,7 @@
 #include <imageprocessing/io/ImageStackDirectoryReader.h>
 #include <volumes/io/Hdf5VolumeStore.h>
 #include <volumes/ExtractLabels.h>
+#include <sopnet/blockwise/persistence/BackendClient.h>
 
 util::ProgramOption optionIntensities(
 		util::_long_name        = "intensities",
@@ -31,6 +32,36 @@ util::ProgramOption optionExtractLabels(
 		util::_description_text = "Indicate that the labeled volume consists of a foreground/background labeling "
 		                          "(dark/bright) and each 4-connected component of foreground represents one region.");
 
+util::ProgramOption optionUseCatsopProject(
+		util::_long_name        = "useCatsopProject",
+		util::_description_text = "Read the intensities and labels from a CATSOP project. "
+		                          "Set the catsop options accordingly.");
+
+util::ProgramOption optionCatsopSegmentationId(
+		util::_long_name        = "catsopSegmentationId",
+		util::_description_text = "The segmentation id used by catsop to refer to a set of stacks and "
+		                          "extracted structures.");
+
+util::ProgramOption optionCatsopPgHost(
+		util::_long_name        = "catsopPgHost",
+		util::_description_text = "The postgresql host.",
+		util::_default_value    = "localhost");
+
+util::ProgramOption optionCatsopPgUser(
+		util::_long_name        = "catsopPgUser",
+		util::_description_text = "The postgresql username.",
+		util::_default_value    = "catsop");
+
+util::ProgramOption optionCatsopPgPassword(
+		util::_long_name        = "catsopPgPassword",
+		util::_description_text = "The postgresql password.",
+		util::_default_value    = "catsop");
+
+util::ProgramOption optionCatsopPgDatabase(
+		util::_long_name        = "catsopPgDatabase",
+		util::_description_text = "The postgresql database.",
+		util::_default_value    = "catsop");
+
 util::ProgramOption optionProjectFile(
 		util::_long_name        = "projectFile",
 		util::_short_name       = "p",
@@ -44,18 +75,58 @@ int main(int argc, char** argv) {
 		util::ProgramOptions::init(argc, argv);
 		logger::LogManager::init();
 
-		pipeline::Process<ImageStackDirectoryReader> intensityReader(optionIntensities.as<std::string>());
-		pipeline::Process<ImageStackDirectoryReader> labelReader(optionLabels.as<std::string>());
+		pipeline::Value<ImageStack> intensityStack;
+		pipeline::Value<ImageStack> labelStack;
 
-		pipeline::Value<ImageStack> intensityStack = intensityReader->getOutput();
-		pipeline::Value<ImageStack> labelStack = labelReader->getOutput();
+		if (optionUseCatsopProject) {
+
+			StackDescription rawStackDescription;
+			rawStackDescription.segmentationId = optionCatsopSegmentationId;
+			StackDescription memStackDescription;
+			memStackDescription.segmentationId = optionCatsopSegmentationId;
+
+			ProjectConfiguration configuration;
+			configuration.setBackendType(ProjectConfiguration::PostgreSql);
+			configuration.setPostgreSqlHost(optionCatsopPgHost);
+			configuration.setPostgreSqlUser(optionCatsopPgUser);
+			configuration.setPostgreSqlPassword(optionCatsopPgPassword);
+			configuration.setPostgreSqlDatabase(optionCatsopPgDatabase);
+			configuration.setCatmaidStack(Raw, rawStackDescription);
+			configuration.setCatmaidStack(Membrane, memStackDescription);
+
+			BackendClient backendClient;
+			backendClient.fillProjectConfiguration(configuration);
+
+			// get the complete raw stack
+
+			boost::shared_ptr<StackStore> stackStore =
+					backendClient.createStackStore(configuration, Raw);
+
+			util::box<unsigned int, 3> volumeBox(
+					util::point<unsigned int, 3>(0, 0, 0),
+					configuration.getVolumeSize());
+			LOG_USER(logger::out) << "reading raw images in " << volumeBox << "..." << std::flush;
+			intensityStack = stackStore->getImageStack(volumeBox);
+			LOG_USER(logger::out) << " done." << std::endl;
+
+			// DEBUG
+			return 0;
+
+		} else {
+
+			pipeline::Process<ImageStackDirectoryReader> intensityReader(optionIntensities.as<std::string>());
+			pipeline::Process<ImageStackDirectoryReader> labelReader(optionLabels.as<std::string>());
+
+			intensityStack = intensityReader->getOutput();
+			labelStack = labelReader->getOutput();
+		}
 
 		if (optionExtractLabels) {
 
 			LOG_DEBUG(logger::out) << "[main] extracting labels from connected components" << std::endl;
 
 			pipeline::Process<ExtractLabels> extractLabels;
-			extractLabels->setInput(labelReader->getOutput());
+			extractLabels->setInput(labelStack);
 			labelStack = extractLabels->getOutput();
 		}
 
