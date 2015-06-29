085e678bd652d9b53bb0d32548a19598efa0c185
Jan Funke <funke@ini.ch>
2015-06-29 18:23:38 -0400

    added debugging output and timers to TubeExtractor

--- horst/tubes/TubeExtractor.cpp
+++ horst/tubes/TubeExtractor.cpp
@@ -1,28 +1,49 @@
+#include <util/timing.h>
+#include <util/Logger.h>
 #include <vigra/functorexpression.hxx>
 #include "TubeExtractor.h"
+
+logger::LogChannel tubeextractorlog("tubeextractorlog", "[TubeExtractor] ");
 
 void
 TubeExtractor::extractFrom(ExplicitVolume<int>& labels, const std::set<TubeId>& ids) {
 
 	std::map<TubeId, util::box<float,3>> bbs;
 
-	// get the discrete bounding boxes of all tubes
-	for (unsigned int z = 0; z < labels.depth();  z++)
-	for (unsigned int y = 0; y < labels.height(); y++)
-	for (unsigned int x = 0; x < labels.width();  x++) {
+	{
+		UTIL_TIME_SCOPE("computing bounding boxes");
 
-		TubeId id = labels(x, y, z);
+		// get the discrete bounding boxes of all tubes
+		for (unsigned int z = 0; z < labels.depth();  z++)
+		for (unsigned int y = 0; y < labels.height(); y++)
+		for (unsigned int x = 0; x < labels.width();  x++) {
 
-		if (id == 0)
-			continue;
+			TubeId id = labels(x, y, z);
 
-		bbs[id] += util::box<float,3>(x, y, z, x+1, y+1, z+1);
+			if (id == 0)
+				continue;
+
+			bbs[id] += util::box<float,3>(x, y, z, x+1, y+1, z+1);
+		}
 	}
 
+	unsigned int numTubes = bbs.size();
+
+	LOG_USER(tubeextractorlog) << "found " << numTubes << " tubes" << std::endl;
+
+	unsigned int i = 0;
 	for (auto& p : bbs) {
+
+		if (i % 10 == 0)
+		LOG_USER(tubeextractorlog)
+				<< logger::delline << "extracting volumes... "
+				<< ((float)i/numTubes)*100 << "%"
+				<< std::flush;
 
 		TubeId                    id = p.first;
 		const util::box<float,3>& bb = p.second;
+
+		LOG_DEBUG(tubeextractorlog) << "extracting tube " << id << std::endl;
 
 		if (!ids.empty())
 			if (!ids.count(id))
@@ -37,14 +58,22 @@ TubeExtractor::extractFrom(ExplicitVolume<int>& labels, const std::set<TubeId>& 
 				labels.getResolutionZ());
 		volume.setOffset(labels.getOffset() + bb.min()*labels.getResolution());
 
-		// copy data
-		vigra::transformMultiArray(
-				labels.data().subarray(
-						vigra::Shape3(bb.min().x(), bb.min().y(), bb.min().z()),
-						vigra::Shape3(bb.max().x(), bb.max().y(), bb.max().z())),
-				volume.data(),
-				(vigra::functor::Arg1() == vigra::functor::Param(id)));
+		{
+			UTIL_TIME_SCOPE("extract tube volume");
 
-		_store->saveVolume(id, volume);
+			// copy data
+			vigra::transformMultiArray(
+					labels.data().subarray(
+							vigra::Shape3(bb.min().x(), bb.min().y(), bb.min().z()),
+							vigra::Shape3(bb.max().x(), bb.max().y(), bb.max().z())),
+					volume.data(),
+					(vigra::functor::Arg1() == vigra::functor::Param(id)));
+		}
+
+		{
+			UTIL_TIME_SCOPE("save tube volume");
+
+			_store->saveVolume(id, volume);
+		}
 	}
 }
