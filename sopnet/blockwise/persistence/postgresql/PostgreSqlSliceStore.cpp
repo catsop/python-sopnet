d6615111208f03adfb70561744453d0ad8c7bc31
Andrew Champion <andrew.champion@gmail.com>
2016-06-11 14:23:22 -0400

    Factor endian headers into util

--- sopnet/blockwise/persistence/postgresql/PostgreSqlSliceStore.cpp
+++ sopnet/blockwise/persistence/postgresql/PostgreSqlSliceStore.cpp
@@ -2,35 +2,8 @@
 #ifdef HAVE_PostgreSQL
 
 #include "PostgreSqlUtils.h"
+
 #include <boost/tokenizer.hpp>
-#include <arpa/inet.h>
-
-#ifdef __APPLE__
-#include <machine/endian.h>
-#include <libkern/OSByteOrder.h>
-
-#define htobe16(x) OSSwapHostToBigInt16(x)
-#define htole16(x) OSSwapHostToLittleInt16(x)
-#define be16toh(x) OSSwapBigToHostInt16(x)
-#define le16toh(x) OSSwapLittleToHostInt16(x)
-
-#define htobe32(x) OSSwapHostToBigInt32(x)
-#define htole32(x) OSSwapHostToLittleInt32(x)
-#define be32toh(x) OSSwapBigToHostInt32(x)
-#define le32toh(x) OSSwapLittleToHostInt32(x)
-
-#define htobe64(x) OSSwapHostToBigInt64(x)
-#define htole64(x) OSSwapHostToLittleInt64(x)
-#define be64toh(x) OSSwapBigToHostInt64(x)
-#define le64toh(x) OSSwapLittleToHostInt64(x)
-
-#define __BIG_ENDIAN    BIG_ENDIAN
-#define __LITTLE_ENDIAN LITTLE_ENDIAN
-#define __BYTE_ORDER    BYTE_ORDER
-#else
-#include <endian.h>
-#endif
-
 #include <boost/timer/timer.hpp>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
@@ -41,6 +14,7 @@
 #include <imageprocessing/ConnectedComponent.h>
 #include <slices/ComponentTreeConverter.h>
 #include <util/ProgramOptions.h>
+#include <util/endian.h>
 #include <util/httpclient.h>
 #include <util/box.hpp>
 #include <util/point.hpp>
