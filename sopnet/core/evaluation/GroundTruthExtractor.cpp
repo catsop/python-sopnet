c8a613b6fcfb35ca51ad07bf713ff20c0cc668b6
Andrew Champion <andrew.champion@gmail.com>
2016-06-11 20:04:01 -0400

    Import GroundTruthExtractor from funkey/sopnet
    Written by Jan Funke.

---
+++ sopnet/core/evaluation/GroundTruthExtractor.cpp
@@ -0,0 +1,290 @@
+#include <pipeline/Value.h>
+#include <pipeline/Process.h>
+#include <imageprocessing/ImageExtractor.h>
+#include <sopnet/slices/SliceExtractor.h>
+#include "GroundTruthExtractor.h"
+
+util::ProgramOption optionGroundTruthFromSkeletons(
+		util::_module           = "sopnet.evaluation",
+		util::_long_name        = "groundTruthFromSkeletons",
+		util::_description_text = "Indicate that the ground truth images contain skeletons and not volumetric processes.");
+
+util::ProgramOption optionGroundTruthAddIntensityBoundaries(
+		util::_module           = "sopnet.evaluation",
+		util::_long_name        = "groundTruthAddIntensityBoundaries",
+		util::_description_text = "Separate ground truth regions of different intensities with a black boundary, such that components of same intensity end up to be exactly one slice.");
+
+logger::LogChannel groundtruthextractorlog("groundtruthextractorlog", "[GroundTruthExtractor] ");
+
+GroundTruthExtractor::GroundTruthExtractor(bool endSegmentsOnly) :
+	_groundTruthSegments(new Segments()),
+	_addIntensityBoundaries(optionGroundTruthAddIntensityBoundaries && !optionGroundTruthFromSkeletons),
+	_endSegmentsOnly(endSegmentsOnly) {
+
+	registerInput(_groundTruthSections, "ground truth sections");
+	registerOutput(_groundTruthSegments, "ground truth segments");
+}
+
+void
+GroundTruthExtractor::updateOutputs() {
+
+	unsigned int firstSection = 0;
+	unsigned int lastSection  = _groundTruthSections->size() - 1;
+
+	LOG_ALL(groundtruthextractorlog)
+			<< "extacting groundtruth from sections "
+			<< firstSection << " - " << lastSection
+			<< std::endl;
+
+	std::vector<Slices> slices = extractSlices(firstSection, lastSection);
+
+	*_groundTruthSegments = findMinimalTrees(slices);
+}
+
+std::vector<Slices>
+GroundTruthExtractor::extractSlices(int firstSection, int lastSection) {
+
+	// find the maximal value in the ground truth images
+	float maxIntensity = 0;
+	foreach (boost::shared_ptr<Image> image, *_groundTruthSections) {
+
+		float min, max;
+		image->minmax(&min, &max);
+
+		maxIntensity = std::max(maxIntensity, max);
+	}
+
+	// create parameters suitable to extract ground-truth connected components
+	pipeline::Value<ComponentTreeExtractorParameters> cteParameters;
+	if (optionGroundTruthFromSkeletons)
+		cteParameters->minSize  = 0; // skeletons are small
+	else
+		cteParameters->minSize  = 50; // this is to avoid this tiny annotation that mess up the result
+	cteParameters->maxSize      = 10000000;
+	cteParameters->darkToBright = false;
+	cteParameters->sameIntensityComponents = _addIntensityBoundaries; // only extract connected components of same intensity
+	cteParameters->minIntensity = 0;
+	cteParameters->maxIntensity = maxIntensity;
+
+	// create a section extractor to access the sections in the stack
+	pipeline::Process<ImageExtractor> sectionExtractor;
+	sectionExtractor->setInput(_groundTruthSections);
+
+	// list of all slices for each section
+	std::vector<Slices> slices;
+
+	float resX = _groundTruthSections->getResolutionX();
+	float resY = _groundTruthSections->getResolutionY();
+	float resZ = _groundTruthSections->getResolutionZ();
+
+	for (int section = firstSection; section <= lastSection; section++) {
+
+		LOG_DEBUG(groundtruthextractorlog) << "extracting slices in section " << section << std::endl;
+
+		// create a SliceExtractor
+		pipeline::Process<SliceExtractor<unsigned short> > sliceExtractor(section, resX, resY, resZ, false /* don't downsample */);
+
+		// give it the section it has to process and our parameters
+		sliceExtractor->setInput("membrane", sectionExtractor->getOutput(section));
+		sliceExtractor->setInput("parameters", cteParameters);
+
+		// get the slices in the current section
+		pipeline::Value<Slices> sectionSlices = sliceExtractor->getOutput("slices");
+		slices.push_back(*sectionSlices);
+
+		LOG_ALL(groundtruthextractorlog) << "found " << sectionSlices->size() << " slices" << std::endl;
+	}
+
+	return slices;
+}
+
+Segments
+GroundTruthExtractor::findMinimalTrees(const std::vector<Slices>& slices) {
+
+	// tree segments of all found neurons
+	Segments segments;
+
+	segments.setResolution(
+			_groundTruthSections->getResolutionX(),
+			_groundTruthSections->getResolutionY(),
+			_groundTruthSections->getResolutionZ());
+
+	// all possible continuation segments by neuron label
+	std::map<float, std::vector<ContinuationSegment> > links;
+
+	// number of links to left and right for each slice
+	std::map<unsigned int, unsigned int> linksLeft;
+	std::map<unsigned int, unsigned int> linksRight;
+
+	// fill links
+	links = extractContinuations(slices);
+
+	// for each neuron label
+	if (!_endSegmentsOnly) {
+
+		typedef std::map<float, std::vector<ContinuationSegment> >::value_type pair_t;
+		foreach (pair_t& pair, links) {
+
+			float label = pair.first;
+			std::vector<ContinuationSegment>& continuations = pair.second;
+
+			findLabelTree(label, continuations, linksLeft, linksRight, segments);
+		}
+
+	} else {
+
+		LOG_USER(groundtruthextractorlog) << "skipping extraction of continuation segments" << std::endl;
+	}
+
+	// postprocessing, for each slice
+	for (unsigned int i = 0; i < slices.size(); i++)
+		foreach (boost::shared_ptr<Slice> slice, slices[i]) {
+
+			// if slice is used from one side only, add an end segment
+			if (linksLeft[slice->getId()] == 0)
+				segments.add(boost::make_shared<EndSegment>(Segment::getNextSegmentId(), Left, slice));
+			if (linksRight[slice->getId()] == 0)
+				segments.add(boost::make_shared<EndSegment>(Segment::getNextSegmentId(), Right, slice));
+		}
+
+	return segments;
+}
+
+void
+GroundTruthExtractor::findLabelTree(
+		float label,
+		std::vector<ContinuationSegment>& continuations,
+		std::map<unsigned int, unsigned int>& linksLeft,
+		std::map<unsigned int, unsigned int>& linksRight,
+		Segments& segments) {
+
+	LOG_ALL(groundtruthextractorlog) << "processing neuron label " << label << std::endl;
+
+	// all currently connected slices
+	std::set<unsigned int> connectedSlices;
+
+	// sort continuations by overlap
+	std::sort(continuations.begin(), continuations.end(), ContinuationComparator());
+
+	// pick initial slice, put into connected slices
+	unsigned int initialSlice = continuations.begin()->getSourceSlice()->getId();
+	connectedSlices.insert(initialSlice);
+
+	LOG_ALL(groundtruthextractorlog) << "initial slice is " << initialSlice << std::endl;
+
+	// iterate
+	bool allConnected = false;
+	while (!allConnected) {
+
+		bool foundOpenEdge = false;
+		bool foundDisconnectedSlices = false;
+
+		// find cheapest continuation for any connected slice to any not 
+		// connected slice
+		for (std::vector<ContinuationSegment>::const_iterator i = continuations.begin(); i != continuations.end(); i++) {
+
+			unsigned int source = i->getSourceSlice()->getId();
+			unsigned int target = i->getTargetSlice()->getId();
+
+			int sourceTargetCount = connectedSlices.count(source) + connectedSlices.count(target);
+
+			if (sourceTargetCount != 1) {
+
+				if (sourceTargetCount == 0)
+					foundDisconnectedSlices = true;
+
+				continue;
+			}
+
+			LOG_ALL(groundtruthextractorlog)
+					<< "next best continuation from connected to not-connected slice is "
+					<< source << " -> "
+					<< target << std::endl;
+
+			foundOpenEdge = true;
+
+			// put continuation in segments
+			segments.add(boost::make_shared<ContinuationSegment>(*i));
+
+			// count number of usages of involved slices
+			if (i->getDirection() == Right) {
+
+				linksLeft[target]++;
+				linksRight[source]++;
+
+			} else {
+
+				linksRight[target]++;
+				linksLeft[source]++;
+			}
+
+			// put new slice into connected slices
+			connectedSlices.insert(target);
+			connectedSlices.insert(source);
+
+			break;
+		}
+
+		if (!foundOpenEdge && foundDisconnectedSlices) {
+
+			LOG_USER(groundtruthextractorlog) << "Warning: ground-truth contains disconnected neuron with same label: " << label << std::endl;
+
+			// add a new disconnected slice to the set of connected slices 
+			// to continue growing the tree
+			for (std::vector<ContinuationSegment>::const_iterator i = continuations.begin(); i != continuations.end(); i++) {
+
+				if (connectedSlices.count(i->getSourceSlice()->getId()) == 0) {
+
+					connectedSlices.insert(i->getSourceSlice()->getId());
+					break;
+				}
+
+				if (connectedSlices.count(i->getTargetSlice()->getId()) == 0) {
+
+					connectedSlices.insert(i->getTargetSlice()->getId());
+					break;
+				}
+			}
+
+			continue;
+		}
+
+		if (!foundOpenEdge)
+			allConnected = true;
+	}
+}
+
+std::map<float, std::vector<ContinuationSegment> >
+GroundTruthExtractor::extractContinuations(const std::vector<Slices>& slices) {
+
+	std::map<float, std::vector<ContinuationSegment> > continuations;
+
+	for (unsigned int i = 0; i < slices.size() - 1; i++) {
+
+		LOG_ALL(groundtruthextractorlog) << "extracting potential continuations between " << i << " and " << (i+1) << std::endl;
+
+		foreach (boost::shared_ptr<Slice> leftSlice, slices[i]) {
+
+			float leftValue = leftSlice->getComponent()->getValue();
+
+			foreach (boost::shared_ptr<Slice> rightSlice, slices[i+1]) {
+
+				float rightValue = rightSlice->getComponent()->getValue();
+
+				if (leftValue == rightValue) {
+
+					LOG_ALL(groundtruthextractorlog) << "found a potential continuation" << std::endl;
+
+					continuations[leftValue].push_back(
+							ContinuationSegment(
+									Segment::getNextSegmentId(),
+									Right,
+									leftSlice,
+									rightSlice));
+				}
+			}
+		}
+	}
+
+	return continuations;
+}
---
+++ sopnet/core/evaluation/GroundTruthExtractor.h
@@ -0,0 +1,90 @@
+#ifndef SOPNET_GROUND_TRUTH_EXTRACTOR_H__
+#define SOPNET_GROUND_TRUTH_EXTRACTOR_H__
+
+#include <vector>
+
+#include <pipeline/SimpleProcessNode.h>
+#include <imageprocessing/ImageStack.h>
+#include <sopnet/slices/Slices.h>
+#include <sopnet/segments/Segments.h>
+#include <sopnet/features/Overlap.h>
+
+extern util::ProgramOption optionGroundTruthFromSkeletons;
+
+class GroundTruthExtractor : public pipeline::SimpleProcessNode<> {
+
+public:
+
+	/**
+	 * Create a ground truth extractor.
+	 *
+	 * @param endSegmentsOnly
+	 *              Extract only end segments, no continuations. Each slice will 
+	 *              be a "neuron".
+	 */
+	GroundTruthExtractor(bool endSegmentsOnly = false);
+
+private:
+
+	/**
+	 * Sorts continuations by source-target overlap descending.
+	 */
+	struct ContinuationComparator {
+
+		ContinuationComparator() :
+			overlap(false, false) {}
+
+		bool operator()(const ContinuationSegment& a, const ContinuationSegment& b) {
+
+			double overlapA = overlap(*a.getSourceSlice(), *a.getTargetSlice());
+			double overlapB = overlap(*b.getSourceSlice(), *b.getTargetSlice());
+
+			// larger overlap goes to front
+			if (overlapA != overlapB)
+				return overlapA > overlapB;
+
+			util::point<double> diffA = a.getTargetSlice()->getComponent()->getCenter() - a.getSourceSlice()->getComponent()->getCenter();
+			util::point<double> diffB = b.getTargetSlice()->getComponent()->getCenter() - b.getSourceSlice()->getComponent()->getCenter();
+
+			double distanceA = diffA.x*diffA.x + diffA.y*diffA.y;
+			double distanceB = diffB.x*diffB.x + diffB.y*diffB.y;
+
+			// smaller displacement goes to front
+			return distanceA < distanceB;
+		}
+
+		Overlap overlap;
+	};
+
+	void updateOutputs();
+
+	// extract all slices of each ground-truth section
+	std::vector<Slices> extractSlices(int firstSection, int lastSection);
+
+	std::map<float, std::vector<ContinuationSegment> > extractContinuations(const std::vector<Slices>& slices);
+
+	// find a minimal spanning segment tree for each set of slices with the same 
+	// id
+	Segments findMinimalTrees(const std::vector<Slices>& slices);
+
+	// find on tree of segments per connected component of label
+	void findLabelTree(
+			float label,
+			std::vector<ContinuationSegment>& continuations,
+			std::map<unsigned int, unsigned int>& linksLeft,
+			std::map<unsigned int, unsigned int>& linksRight,
+			Segments& segments);
+
+	// the ground truth images
+	pipeline::Input<ImageStack> _groundTruthSections;
+
+	// continuation and end segments of the ground-truth
+	pipeline::Output<Segments> _groundTruthSegments;
+
+	bool _addIntensityBoundaries;
+
+	bool _endSegmentsOnly;
+};
+
+#endif // SOPNET_GROUND_TRUTH_EXTRACTOR_H__
+
